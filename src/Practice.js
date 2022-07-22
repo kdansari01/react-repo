675c6079eba13271303b1d2c0a0204903abc455c
khusmuddin <khusmuddin.ansari@gmail.com>
2022-07-22 11:25:20 -0400

    modified fetch api projct

--- src/Practice.js
+++ src/Practice.js
@@ -1,8 +1,10 @@
 import { useEffect, useState } from "react";
-import "./App.css"
+import "./App.css";
 
 export const Practice = () => {
-  const [data, setData] = useState([]);
+  const [data, setData] = useState([]); //stored data
+  const [count, setCount] = useState(1); //set count for next and prev
+  const [index1, setIndex] = useState(1);
   // const [newData, setNewData] = useState([]);
 
   useEffect(() => {
@@ -13,54 +15,67 @@ export const Practice = () => {
       });
   }, []);
 
+  //prev button
+
   console.log(data);
-  // const handleBtn = () => {
-  //   setNewData(data);
-  //   // console.log(newData);
-  // };
 
+  const prevItem = () => {
+    if (count !== 0) {
+      setCount(count - 1);
+    }
+    if (count === 0) {
+      alert("prev is null");
+    }
+  };
 
-//  const changeData = ()=>{
-  
-//   }
-//  }
+  //next button
+  const nextItem = (e) => {
+    setCount(count + 1);
+  };
 
+  useEffect(() => {
+    if (count < 11) {
+      setIndex(count);
+    }
+    if (count === 10) {
+      setCount(1);
+    }
+  }, [count]);
+  console.log(count);
 
   return (
     <div className="insurance-list">
       <h2 className="heading">Insurance details</h2>
-      {data.map((ele, index) => {
-        if(index===0){
-
-          return (
-            <>
+      {data.map(
+        (ele, index) =>
+          ele.id === index1 && (
+            <div>
+              {console.log(ele.id, index)}
               <div className="list">
                 <div className="list-box" key={ele.id}>
-  
-                  <h2 key={ele.id} className="name">Name: {ele.name}</h2>
-                  <p key={ele.id}>username: {ele.username}</p>
-                  <p key={ele.id}>Email: {ele.email}</p>
+                  <h2 className="name">Name: {ele.name}</h2>
+                  <p>username: {ele.username}</p>
+                  <p>Email: {ele.email}</p>
                   <h4 className="address">address</h4>
-                  <p key={ele.id}> street: {ele.address.street}</p>
-                  <p key={ele.id}>city: {ele.address.city}</p>
-                  <p key={ele.id}>zipcode: {ele.address.zipcode}</p>
-                  <p key={ele.id}>website: {ele.website}</p>
-                  <p key={ele.id}>company: {ele.company.bs}</p>
-                  
+                  <p>city: {ele.address.city}</p>
+                  <p>zipcode: {ele.address.zipcode}</p>
+                  <p>website: {ele.website}</p>
+                  <p> street: {ele.address.street}</p>
+                  <p>company: {ele.company.bs}</p>
                 </div>
-               <div className="buttons"><button className="btn">prev</button>
-                <button className="btn">Next</button></div>
+                <div className="buttons">
+                  <button onClick={prevItem} className="btn">
+                  &#8249;
+
+                  </button> 
+                  <button className="btn" onClick={nextItem}>
+                  &#8250;
+                  </button>
+                </div>
               </div>
-            </>
-          );
-        }
-      })}
-      {/* <button onClick={changeData}> Next</button> */}
-      {/* <div>
-        <button className="btn" onClick={handleBtn}>
-          New Drinks
-        </button>
-      </div> */}
+            </div>
+          )
+      )}
     </div>
   );
 };
