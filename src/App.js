b9c95b808b8393551758d110865752088912e7b7
khusmuddin <khusmuddin.ansari@gmail.com>
2022-07-28 10:46:46 -0400

    website

--- src/App.js
+++ src/App.js
@@ -1,33 +1,43 @@
+import { Home } from "./component/Home";
+import { Profile } from "./component/Profile";
+import { ContactUs } from "./component/ContactUs";
 
-import "./App.css";
+import { Login } from "./component/Login";
+import { Navbar } from "./component/Navbar";
+import { BrowserRouter, Routes, Route } from "react-router-dom"; 
+import "./App.css"
+import { SingUp } from "./component/SingUp";
+import { createContext, useState } from "react";
+// import {NameForm} from "./Form"
+
+const userLoginData = { userdata:[], user:{} }
+
+export const GlobalContext = createContext(userLoginData)
+
 function App() {
-  return (
-    <div className="calculator-grid">
-      <div className="output">
-        <div className="previous">1111</div>
-        <div className="current">222</div>
-      </div>
 
-      <button className="span-two">Ac</button>
-      <button >Del</button>
-      <button >/</button>
-      <button >1</button>
-      <button >2</button>
-      <button >3</button>
-      <button >*</button>
-      <button >4</button>
-      <button >5</button>
-      <button >6</button>
-      <button >+</button>
-      <button >7</button>
-      <button >8</button>
-      <button >9</button>
-      <button >-</button>
-      <button >.</button>
-      <button >0</button>
-      <button className="span-two">=</button>
-      
-    </div>
+  const [global, setGlobal] = useState(userLoginData)
+
+  return (
+    <>
+     <GlobalContext.Provider value={{global, setGlobal}}>
+      <BrowserRouter>
+      <Navbar />
+        <Routes>
+          <Route path="/" exact element={<Home />} />
+          <Route path="/Profile" exact element={<Profile/>} />
+          <Route path="/Login" exact element={<Login/>} />
+          <Route path="/SingUp" exact element={<SingUp/>}/>
+          <Route path="/ContactUs" exact element={<ContactUs/>}/>
+
+
+
+          
+        </Routes>
+      </BrowserRouter>
+      </GlobalContext.Provider>
+    </>
+    // <NameForm/>
   );
 }
 
